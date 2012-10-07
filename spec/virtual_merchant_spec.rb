db4ec12d389024861f9a473a3fd93a8f060abdc1
Lee Quarella <lee@lucidfrog.com>
2012-10-07 19:21:58 -0400

    added virtual_merchant_spec

---
+++ spec/virtual_merchant_spec.rb
@@ -0,0 +1,20 @@
+require 'virtual_merchant'
+
+describe VirtualMerchant, "#amount" do
+  it "receives a response from VM" do
+    creds = VMCredentials.new(
+      account_id: 111,
+      user_id: 222,
+      pin: "abc",
+      referer: "https://thisisauri.com")
+    cc = CreditCard.new(
+      name_on_card: "Lee Quarella",
+      number: "1234567890123456",
+      expiration: "0513",
+      security_code: "1234")
+    amount = VMAmount.new(total: 10.99)
+
+    response = VirtualMerchant.charge(cc, amount, creds)
+    response.should be_an_instance_of VMResponse
+  end
+end
