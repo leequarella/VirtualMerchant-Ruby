d43c223b8d70cdd667366d68ed048db0e73cf81f
Lee Quarella <lee@lucidfrog.com>
2012-10-07 19:04:33 -0400

    added credit_card_spec

---
+++ spec/credit_card_spec.rb
@@ -0,0 +1,15 @@
+require 'virtual_merchant/credit_card'
+
+describe CreditCard, "#amount" do
+  it "initiallizes" do
+    cc = CreditCard.new(
+      name_on_card: "Lee Quarella",
+      number: "1234567890123456",
+      expiration: "0513",
+      security_code: "1234")
+    cc.name_on_card.should eq("Lee Quarella")
+    cc.number.should eq("1234567890123456")
+    cc.expiration.should eq("0513")
+    cc.security_code.should eq("1234")
+  end
+end
