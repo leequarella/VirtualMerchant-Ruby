d0092b7ca603e232befdcc7c6eff5807456beae9
Lee Quarella <lee@lucidfrog.com>
2012-10-07 18:50:44 -0400

    better amount spec

--- spec/amount_spec.rb
+++ spec/amount_spec.rb
@@ -7,14 +7,14 @@ describe VMAmount, "#amount" do
     amount.tax.should eq("0.00")
   end
 
-  it "sets tax passed as float to string with 2 decimals" do
-    amount = VMAmount.new(total: 10.99, tax: 0.1)
-    amount.total.should eq("10.99")
+  it "sets values passed as float to string with 2 decimals" do
+    amount = VMAmount.new(total: 10.9, tax: 0.1)
+    amount.total.should eq("10.90")
     amount.tax.should eq("0.10")
   end
 
-  it "sets tax passed as string" do
-    amount = VMAmount.new(total: 10.99, tax: "0.10")
+  it "accepts decimals passed as strings" do
+    amount = VMAmount.new(total: "10.99", tax: "0.10")
     amount.total.should eq("10.99")
     amount.tax.should eq("0.10")
   end
