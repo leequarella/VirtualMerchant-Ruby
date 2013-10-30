198894b0908d48e39c8a5640d276e58187fe7000
unclelarrysvan <screspo988@gmail.com>
2013-10-30 17:52:28 -0400

    Added device_type to credentials and xml

--- lib/virtual_merchant/credentials.rb
+++ lib/virtual_merchant/credentials.rb
@@ -1,14 +1,15 @@
 module VirtualMerchant
   class Credentials
-    attr_accessor :account_id, :user_id, :pin, :referer, :demo, :source, :ksn
+    attr_accessor :account_id, :user_id, :pin, :referer, :demo, :source, :ksn, :device_type
     def initialize(info)
-      @account_id = info[:account_id].to_s
-      @user_id    = info[:user_id].to_s
-      @pin        = info[:pin].to_s
-      @source     = info[:source].to_s
-      @ksn        = info[:ksn].to_s
-      @referer    = info[:referer].to_s
-      @demo       = info[:demo] || false
+      @account_id  = info[:account_id].to_s
+      @user_id     = info[:user_id].to_s
+      @pin         = info[:pin].to_s
+      @source      = info[:source].to_s
+      @ksn         = info[:ksn].to_s
+      @device_type = info[:device_type].to_s
+      @referer     = info[:referer].to_s
+      @demo        = info[:demo] || false
     end
   end
 end
